4feae2b980bc033955bd9b12d0aa86e20cb1bed4
TheGreatNectarine <margal.nik@gmail.com>
2019-02-26 10:09:37 +0200

    annotations

--- src/main/java/com/nikandr/spring/App.java
+++ src/main/java/com/nikandr/spring/App.java
@@ -1,43 +1,28 @@
 package com.nikandr.spring;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class App {
-
     private Person person;
-    private HelloWorld helloWorld;
 
     protected App(Person person) {
         this.person = person;
     }
 
-    public void setHelloWorld(HelloWorld helloWorld) {
-        this.helloWorld = helloWorld;
-    }
-
     public static void main(String[] args) {
-
-        ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfiguration.class);
+        context.refresh();
 
         App app = context.getBean(App.class);
         app.person.sayHelloVitalik();
-
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-        ctx.register(AppConfiguration.class);
-        ctx.refresh();
-
-        Profile profile = ctx.getBean(Profile.class);
+        Profile profile = context.getBean(Profile.class);
         profile.logProfileInfo();
         profile.shareProfile(new Person(
                 "Olex Mazur",
                 123123123,
                 Arrays.asList("Get info from Nikondr", "sleep")));
-        app.helloWorld.printHello();
     }
 
 }
--- src/main/java/com/nikandr/spring/AppConfiguration.java
+++ src/main/java/com/nikandr/spring/AppConfiguration.java
@@ -3,16 +3,29 @@ package com.nikandr.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+
+import java.util.Arrays;
 
 @Configuration
-@ImportResource("classpath:SpringBeans.xml")
 public class AppConfiguration {
     @Autowired
     private Person person;
 
     @Bean
+    public Person getPerson() {
+        return new Person("Nikondr", 123123412, Arrays.asList("Піти в магазин",
+                "Купити пефка",
+                "Попити пефка",
+                "Всьо)"));
+    }
+
+    @Bean
     public Profile personsProfile() {
         return new Profile(person);
+    }
+
+    @Bean
+    public App getApp() {
+        return new App(person);
     }
 }
